1a69b1c1ea1231f6b3a4b849c3d127515919f186
Charles Sal <cbsal.tec@gmail.com>
2025-02-22 15:18:51 -0300

    feat(components): register controller

--- src/view/pages/Register/useRegisterController.ts
+++ src/view/pages/Register/useRegisterController.ts
@@ -1,6 +1,10 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { authService } from "../../../app/services/authService";
+import { useMutation } from "@tanstack/react-query";
+import { SignupParams } from "../../../app/services/authService/signup";
+import toast from "react-hot-toast";
 
 const schema = z.object({
   name: z.string().nonempty("Nome é obrigatório"),
@@ -18,14 +22,26 @@ type FormData = z.infer<typeof schema>;
 
 export function useRegisterController() {
   const {
-    handleSubmit: hookFormSubmit,
+    handleSubmit: hookFormHandleSubmit,
     register,
     formState: { errors },
   } = useForm<FormData>({
     resolver: zodResolver(schema),
   });
 
-  const handleSubmit = hookFormSubmit((data) => {});
+  const { isPending, mutateAsync } = useMutation({
+    mutationFn: async (data: SignupParams) => {
+      return authService.signup(data);
+    },
+  });
 
-  return { handleSubmit, register, errors };
+  const handleSubmit = hookFormHandleSubmit(async (data) => {
+    try {
+      await mutateAsync(data);
+    } catch {
+      toast.error("Ocorreu um erro ao criar a sua conta!");
+    }
+  });
+
+  return { handleSubmit, register, errors, isPending };
 }
