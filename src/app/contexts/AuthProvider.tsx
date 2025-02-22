e3ef933aeda7924464bab3e0ad9cec95825d554a
Charles Sal <cbsal.tec@gmail.com>
2025-02-22 16:57:37 -0300

    feat(authentication): persisted token

---
+++ src/app/contexts/AuthProvider.tsx
@@ -0,0 +1,46 @@
+import { useCallback, useEffect, useState } from "react";
+import { localStorageKeys } from "../config/localStorageKeys";
+import { useQuery } from "@tanstack/react-query";
+import { usersService } from "../services/usersServices";
+import { AuthContext } from "./AuthContext";
+import toast from "react-hot-toast";
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [signedIn, setSignedIn] = useState<boolean>(() => {
+    const storedAccessToken = localStorage.getItem(
+      localStorageKeys.ACCESS_TOKEN
+    );
+
+    return !!storedAccessToken;
+  });
+
+  const { isError } = useQuery({
+    queryKey: ["users", "me"],
+    queryFn: () => usersService.me(),
+    enabled: signedIn,
+  });
+
+  const signin = useCallback((accessToken: string) => {
+    localStorage.setItem(localStorageKeys.ACCESS_TOKEN, accessToken);
+
+    setSignedIn(true);
+  }, []);
+
+  const signout = useCallback(() => {
+    localStorage.removeItem(localStorageKeys.ACCESS_TOKEN);
+    setSignedIn(false);
+  }, []);
+
+  useEffect(() => {
+    if (isError) {
+      toast.error("Sua sessão expirou!");
+      signout();
+    }
+  }, [isError, signout]);
+
+  return (
+    <AuthContext.Provider value={{ signedIn, signin, signout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
